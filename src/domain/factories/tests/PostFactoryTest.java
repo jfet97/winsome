8cf554fd63dbc00f89d01e4997f1ff81e74a346f
Andrea Simone Costa <andrysimo1997@gmail.com>
2021-12-25 19:59:20 +0100

    add tests to PostFactory and HashPassword

---
+++ src/domain/factories/tests/PostFactoryTest.java
@@ -0,0 +1,93 @@
+package domain.factories.tests;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import domain.factories.PostFactory;
+
+public class PostFactoryTest {
+
+  @Test
+  void nullTitle() {
+    var epost = PostFactory.create(null, "Random content", "John Doe");
+    assertEquals(epost.isValid(), false);
+    assertEquals(epost.fold(set -> set.get(0), __ -> ""), "title cannot be null");
+  }
+
+  @Test
+  void maxLengthExceededTitle() {
+    var epost = PostFactory.create(new String(new char[21]).replace('\0', ' '), "Random content", "John Doe");
+    assertEquals(epost.isValid(), false);
+    assertEquals(epost.fold(set -> set.get(0), __ -> ""), "title max length is 20 characters");
+  }
+
+  @Test
+  void emptyTitle() {
+    var epost = PostFactory.create("", "Random content", "John Doe");
+    assertEquals(epost.isValid(), false);
+    assertEquals(epost.fold(set -> set.get(0), __ -> ""), "title cannot be empty");
+  }
+
+  @Test
+  void nullContent() {
+    var epost = PostFactory.create("A title", null, "John Doe");
+    assertEquals(epost.isValid(), false);
+    assertEquals(epost.fold(set -> set.get(0), __ -> ""), "content cannot be null");
+  }
+
+  @Test
+  void maxLengthExceededContent() {
+    var epost = PostFactory.create("A title", new String(new char[501]).replace('\0', ' '), "John Doe");
+    assertEquals(epost.isValid(), false);
+    assertEquals(epost.fold(set -> set.get(0), __ -> ""), "content max length is 500 characters");
+  }
+
+  @Test
+  void emptyContent() {
+    var epost = PostFactory.create("A title", "", "John Doe");
+    assertEquals(epost.isValid(), false);
+    assertEquals(epost.fold(set -> set.get(0), __ -> ""), "content cannot be empty");
+  }
+
+  @Test
+  void nullAuthor() {
+    var epost = PostFactory.create("A title", "Random content", null);
+    assertEquals(epost.isValid(), false);
+    assertEquals(epost.fold(set -> set.get(0), __ -> ""), "author cannot be null");
+  }
+
+  @Test
+  void threeErrors() {
+    var epost = PostFactory.create(null, null, null);
+    assertEquals(epost.isValid(), false);
+    assertEquals(epost.fold(set -> set.foldLeft(0, (tot, curr) -> tot + 1), post -> 0), 3);
+
+    var errors = epost.getError();
+    assertTrue(errors.asJava().contains("title cannot be null"));
+    assertTrue(errors.asJava().contains("content cannot be null"));
+    assertTrue(errors.asJava().contains("author cannot be null"));
+  }
+
+  @Test
+  void validPost() {
+    var epost = PostFactory.create("A title", "Random content", "John Doe");
+    assertEquals(epost.isValid(), true);
+
+    var post = epost.fold(set -> null, x -> x);
+    assertEquals(post.author, "John Doe");
+    assertEquals(post.content, "Random content");
+    assertEquals(post.title, "A title");
+    assertEquals(post.downvotes, 0);
+    assertEquals(post.downvotes, 0);
+    assertTrue(post.comments.isEmpty());
+
+  }
+}
--- src/utils/tests/HashPasswordTest.java
+++ src/utils/tests/HashPasswordTest.java
@@ -1,7 +1,8 @@
 package utils.tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -17,5 +18,15 @@ public class HashPasswordTest {
   })
   void sameHash(String password) {
     assertEquals(HashPassword.hash(password), HashPassword.hash(password));
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @CsvSource({
+      "abcde12345",
+      "password",
+      "123456",
+  })
+  void differentHash(String password) {
+    assertNotEquals(HashPassword.hash(password), HashPassword.hash(password + " "));
   }
 }

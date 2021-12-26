b3e7552c45c6e4c1f7ce7292200b2d75eccaf159
Andrea Simone Costa <andrysimo1997@gmail.com>
2021-12-26 14:22:56 +0100

    add HttpRequest builder and parser class

---
+++ src/http/request/HttpRequest.java
@@ -0,0 +1,196 @@
+package http.request;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.vavr.control.Either;
+
+public class HttpRequest {
+
+  private String method = "";
+  private String requestTarget = "";
+  private String HTTPVersion = "";
+  private Map<String, String> headers = new HashMap<String, String>();
+  private String body = "";
+
+  private HttpRequest() {
+  }
+
+  public static final String HTTPV11 = "HTTP/1.1";
+  public static final String HTTPV20 = "HTTP/2.0";
+  public static final String CRLF = "\r\n";
+
+  public static Either<String, HttpRequest> build(String method) {
+    HttpRequest instance = new HttpRequest();
+    var errorMessage = "";
+
+    if (method == null) {
+      errorMessage = "HTTP method cannot be null";
+    } else {
+      switch (method) {
+        case "GET":
+        case "POST":
+        case "PUT":
+        case "PATCH":
+        case "DELETE": {
+          instance.method = method;
+          break;
+        }
+        default: {
+          errorMessage = method + " is not yet supported by HttpRequest";
+          break;
+        }
+      }
+    }
+
+    if (!errorMessage.equals("")) {
+      return Either.left(errorMessage);
+    } else {
+      return Either.right(instance);
+    }
+  }
+
+  public static Either<String, HttpRequest> parse(String request) {
+    var instance = new HttpRequest();
+    var errorMessage = "";
+
+    if (request == null) {
+      errorMessage = "HTTP request cannot be null";
+    } else {
+      try {
+
+        var requestAndBody = request.split(CRLF + CRLF);
+        var requestBeforeBodyEntries = requestAndBody[0].split(CRLF);
+
+        // parse request line
+        var requestLineEntries = requestBeforeBodyEntries[0].split(" ");
+        instance.method = requestLineEntries[0];
+        instance.requestTarget = requestLineEntries[1];
+        instance.HTTPVersion = requestLineEntries[2];
+
+        switch (instance.method) {
+          case "GET":
+          case "POST":
+          case "PUT":
+          case "PATCH":
+          case "DELETE": {
+            break;
+          }
+          default: {
+            errorMessage = instance.method + " is not yet supported by HttpRequest";
+          }
+        }
+
+        // parse headers
+        if (errorMessage.equals("")) {
+          Arrays.stream(requestBeforeBodyEntries).skip(1).forEach(header -> {
+            var keyValue = header.split(": ");
+            instance.headers.put(keyValue[0], keyValue[1]);
+          });
+        }
+
+        // parse body
+        if (errorMessage.equals("")) {
+          if (requestAndBody.length > 1) {
+            instance.body = requestAndBody[1];
+          }
+        }
+
+      } catch (Exception e) {
+        errorMessage = "invalid or not supported HTTP request:\n" + e.getMessage();
+      }
+    }
+
+    if (!errorMessage.equals("")) {
+      return Either.left(errorMessage);
+    } else {
+      return Either.right(instance);
+    }
+  }
+
+  public Either<String, HttpRequest> setRequestTarget(String requestTarget) {
+    if (requestTarget != null) {
+      this.requestTarget = requestTarget;
+      return Either.right(this);
+    } else {
+      return Either.left("HTTP request target cannot be null");
+    }
+  }
+
+  public Either<String, HttpRequest> setHTTPVersion(String HTTPVersion) {
+    if (HTTPVersion != null) {
+      this.HTTPVersion = HTTPVersion;
+      return Either.right(this);
+    } else {
+      return Either.left("HTTP request version cannot be null");
+    }
+  }
+
+  public Either<String, HttpRequest> setHeader(String key, String value) {
+    if (key != null && value != null) {
+      this.headers.put(key, value);
+      return Either.right(this);
+    } else {
+      return Either.left("HTTP request header's key and value cannot be null");
+    }
+  }
+
+  public Either<String, HttpRequest> deleteHeader(String key) {
+    if (key != null) {
+      this.headers.remove(key);
+      return Either.right(this);
+    } else {
+      return Either.left("HTTP request header's key to delete cannot be null");
+    }
+  }
+
+  public Either<String, HttpRequest> setBody(String body) {
+    if (body != null) {
+      this.body = body;
+      return Either.right(this);
+    } else {
+      return Either.left("HTTP request body cannot be null");
+    }
+  }
+
+  public boolean isValid() {
+    var isValid = true;
+
+    // always mandatory
+    if (this.method.equals("") || this.requestTarget.equals("") || this.HTTPVersion.equals("")) {
+      isValid = false;
+    }
+
+    // From MDN:
+    // Sending body/payload in a GET request may cause some existing implementations
+    // to reject the request — while not prohibited by the specification, the
+    // semantics are undefined. It is better to just avoid sending payloads in GET
+    // requests.
+    if (this.method.equals("GET") && !this.body.equals("")) {
+      isValid = false;
+    }
+
+    if (!this.method.equals("GET") && this.body.equals("")) {
+      isValid = false;
+    }
+
+    return isValid;
+  }
+
+  @Override
+  public String toString() {
+
+    var request = this.method + " " + this.requestTarget + " " + this.HTTPVersion + CRLF;
+
+    for (var entry : this.headers.entrySet()) {
+      request += entry.getKey() + ": " + entry.getValue() + CRLF;
+    }
+
+    request += CRLF;
+
+    request += this.body;
+
+    return request;
+  }
+}
---
+++ src/http/request/tests/HttpRequestTest.java
@@ -0,0 +1,88 @@
+package http.request.tests;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import http.request.HttpRequest;
+
+public class HttpRequestTest {
+
+  private static final String CRLF = "\r\n";
+
+  private static final String validGET = "" +
+      "GET /test/index.html HTTP/1.1" + CRLF +
+      "User-Agent: Mozilla/4.0(compatible; MSIE5.01; Windows NT)" + CRLF +
+      "Host: www.tutorialspoint.com" + CRLF +
+      "Accept-Language: en-us" + CRLF +
+      "Accept-Encoding: gzip, deflate" + CRLF +
+      "Connection: Keep-Alive" + CRLF +
+      CRLF;
+
+  private static final String validPOST = "" +
+      "POST /test/index.html HTTP/1.1" + CRLF +
+      "User-Agent: Mozilla/4.0(compatible; MSIE5.01; Windows NT)" + CRLF +
+      "Host: www.tutorialspoint.com" + CRLF +
+      "Accept-Language: en-us" + CRLF +
+      "Accept-Encoding: gzip, deflate" + CRLF +
+      "Connection: Keep-Alive" + CRLF +
+      CRLF +
+      "{\"field\":\"property\",\"array\":[1,2,{\"three\":3}]}";
+
+  @Test
+  void parseValidGET() {
+    var erequest = HttpRequest.parse(validGET);
+    assertTrue(erequest.isRight());
+
+    var request = erequest.get();
+    assertTrue(request.isValid());
+    System.out.println(request);
+  }
+
+  @Test
+  void parseValidPOST() {
+    var erequest = HttpRequest.parse(validPOST);
+    assertTrue(erequest.isRight());
+
+    var request = erequest.get();
+    assertTrue(request.isValid());
+    System.out.println(request);
+  }
+
+  @Test
+  void buildValidGET() {
+    var erequest = HttpRequest.build("GET")
+        .flatMap(req -> req.setRequestTarget("/test/index.html"))
+        .flatMap(req -> req.setHTTPVersion(HttpRequest.HTTPV11))
+        .flatMap(req -> req.setHeader("User-Agent", "Mozilla/4.0(compatible; MSIE5.01; Windows NT)"))
+        .flatMap(req -> req.setHeader("Host", "www.tutorialspoint.com"))
+        .flatMap(req -> req.setHeader("Accept-Language", "en-us"))
+        .flatMap(req -> req.setHeader("Accept-Encoding", "gzip, deflate"))
+        .flatMap(req -> req.setHeader("Connection", "Keep-Alive"));
+
+    assertTrue(erequest.isRight());
+
+    var request = erequest.get();
+    assertTrue(request.isValid());
+    System.out.println(request);
+  }
+
+  @Test
+  void buildValidPOST() {
+    var erequest = HttpRequest.build("POST")
+        .flatMap(req -> req.setRequestTarget("/test/index.html"))
+        .flatMap(req -> req.setHTTPVersion(HttpRequest.HTTPV11))
+        .flatMap(req -> req.setHeader("User-Agent", "Mozilla/4.0(compatible; MSIE5.01; Windows NT)"))
+        .flatMap(req -> req.setHeader("Host", "www.tutorialspoint.com"))
+        .flatMap(req -> req.setHeader("Accept-Language", "en-us"))
+        .flatMap(req -> req.setHeader("Accept-Encoding", "gzip, deflate"))
+        .flatMap(req -> req.setHeader("Connection", "Keep-Alive"))
+        .flatMap(req -> req.setBody("{\"field\":\"property\",\"array\":[1,2,{\"three\":3}]}"));
+
+    assertTrue(erequest.isRight());
+
+    var request = erequest.get();
+    assertTrue(request.isValid());
+    System.out.println(request);
+  }
+}

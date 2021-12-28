74ecab08a3078a2c328d32cefa590ac795c70e4b
Andrea Simone Costa <andrysimo1997@gmail.com>
2021-12-28 10:28:10 +0100

    add simple Error Pojo

---
+++ src/domain/pojos/Error.java
@@ -0,0 +1,21 @@
+package domain.pojos;
+
+public class Error {
+
+  public String message;
+
+  public static Error of(String message) {
+    var instance = new Error();
+
+    instance.message = message != null ? message : "";
+
+    return instance;
+  }
+
+  public String toJSON() {
+
+    return "{\"message\":\"" + this.message + "\"}";
+
+  }
+
+}
